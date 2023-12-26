afc66e7c9ce20a659bfdd244c68a5a56e710d6d8
Robert Claus <robertclaus@gmail.com>
2023-12-26 09:23:31 -0800

    Add google API disclosure

--- app/footer.jsx
+++ app/footer.jsx
@@ -1,8 +1,14 @@
-
-
 export default function Footer() {
-    return <div className="flex justify-around py-4 text-xs w-1/2 m-auto">
+  return (
+    <div>
+      <div className="flex justify-around py-4 text-xs w-1/2 m-auto">
         <a href="/privacy.html">Privacy</a>
         <a href="/terms.html">Terms</a>
+      </div>
+      <div className="text-center p-6 text-xs">
+        Email Purge Assistant use and transfer of information received from Google APIs 
+        to any other app will adhere to <a className="underline text-blue-600 hover:text-blue-800 visited:text-purple-600"   href="https://developers.google.com/terms/api-services-user-data-policy#additional_requirements_for_specific_api_scopes">Google API Services User Data Policy</a>, 
+        including the Limited Use requirements.</div>
     </div>
-}   
+  );
+}
